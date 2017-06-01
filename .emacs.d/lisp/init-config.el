4ef26aa28f4843e382be3a23b8fbfaabc7433c46
wumin.sunland <fusu1435@163.com>
2017-06-01 09:04:49 +0800

    Config evil-leader & evil-nerd-commenter

--- .emacs.d/lisp/init-config.el
+++ .emacs.d/lisp/init-config.el
@@ -26,7 +26,46 @@
 
 ;;
 
+;; config evil-leader
+(global-evil-leader-mode)
+(evil-leader/set-key
+  "e" 'find-file
+  "b" 'switch-to-buffer
+  "k" 'kill-buffer)
+
 ;; config evil-matchit
 (global-evil-matchit-mode 1)
 
+;; config evil-nerd-commenter
+;; Emacs key bindings
+(global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+(global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
+(global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
+(global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
+
+;; Vim key bindings
+(require 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-key
+  "ci" 'evilnc-comment-or-uncomment-lines
+  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "cc" 'evilnc-copy-and-comment-lines
+  "cp" 'evilnc-comment-or-uncomment-paragraphs
+  "cr" 'comment-or-uncomment-region
+  "cv" 'evilnc-toggle-invert-comment-line-by-line
+  "."  'evilnc-copy-and-comment-operator
+  "\\" 'evilnc-comment-operator ; if you prefer backslash key
+)
+
+(add-hook 'c-mode-common-hook
+  (lambda ()
+    ;; Preferred comment style
+    (setq comment-start "// "
+          comment-end "")))
+(add-hook 'java-mode-common-hook
+  (lambda ()
+    ;; Preferred comment style
+    (setq comment-start "/* "
+          comment-end " */")))		  
 (provide 'init-config)
